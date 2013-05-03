ae7b293219c21a489d76e5c27041f91ca77ebaea
David Lachut <dslachut@gmail.com>
2013-05-03 14:32:11 -0400

    work on scoreboard

--- Simulation/Simclass.py
+++ Simulation/Simclass.py
@@ -1,6 +1,28 @@
+from Scoreboard import Scoreboard
+from Memory import Memory
+
 class Simulation:
-    def __init__(self,instFName,dataFName,regFName,configFname):
-        print 'Init'
+    def __init__(self,instFname,dataFname,regFname,configFname):
+        print 'Setting up Simulation'
+        FU = []
+        reg = []
+        data = []
+        inst = []
+        with open(instFname) as FILE:
+            inst = FILE.readlines()
+        with open(dataFname) as FILE:
+            lines = FILE.readlines()
+            data = map(lines, lambda x: int(x,2))
+        with open(regFname) as FILE:
+            lines = FILE.readlines()
+            reg = map(lines, lambda x: int(x,2))
+        with open(configFname) as FILE:
+            for line in FILE:
+                FU.append(int(line))
+        self.memory = Memory(data,inst)
+        self.scoreboard = Scoreboard(FU,reg,self.memory)
     
     def Run(self):
-        print 'run'
+        done = False
+        while not done:
+            done = self.scoreboard.Cycle()
